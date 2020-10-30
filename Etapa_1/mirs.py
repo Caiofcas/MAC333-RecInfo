91bc788f4258fb07516a26a1a0c8df3ad817c9e7
Caio <ccastro@localhost.localdomain>
2020-10-30 11:41:48 -0300

    add argument parsing for mirs

---
+++ Etapa_1/mirs.py
@@ -0,0 +1,19 @@
+import argparse
+import re
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Searchs <dir>'s Information Retrieval system.")
+
+    parser.add_argument('dir', help='directory to be processed')
+    parser.add_argument('-t', type=int,
+                        help='Print the <t> most common tokens')
+    parser.add_argument('-r', type=re.compile,
+                        help='Print tokens that match the regex passed')
+    parser.add_argument('-R', type=re.compile,
+                        help='Print tokens that DON\'T match the regex passed')
+
+    args = parser.parse_args()
+    print(args)
